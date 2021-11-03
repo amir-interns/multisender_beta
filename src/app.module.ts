dc05e74bfdaa4e8c3119adbdb0db416880c5f746
Andrew Samoylov <57410309+desmond0002@users.noreply.github.com>
2021-11-03 16:58:12 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { BlockchainModule } from './blockchain/blockchain.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
-import configuration from 'config/configuration';
-import * as path from 'path'
+import  database  from 'config/database'
 import { BlockchainEntity } from '../bd/src/entity/blockchain.entity';
 import {Auth} from "../bd/src/entity/Auth";
 import {AuthModule} from "./auth/auth.module";
@@ -14,16 +13,16 @@ import {AuthModule} from "./auth/auth.module";
 @Module({
   imports: [
             TypeOrmModule.forRootAsync({
-              imports: [ConfigModule],
+              imports: [ConfigModule.forRoot({ load: [database], envFilePath: '.development.env' })],
               useFactory: (configService: ConfigService) => ({
                 type: 'postgres',
-                host: configService.get('host', 'localhost'),
-                port: configService.get<number>('port', 5432),
-                username: configService.get('username', 'postgres'),
-                password: configService.get('password', 'postgres'),
-                database: configService.get('database', 'bd'),
+                host: configService.get<string>('database.host'),
+                port: configService.get<number>('database.port'),
+                username: configService.get<string>('database.username'),
+                password: configService.get<string>('database.password'),
+                database: configService.get<string>('database.database'),
                 entities: [BlockchainEntity, Auth],
-                synchronize: configService.get('synchronize'),
+                synchronize: configService.get<boolean>('database.synchronize'),
               }),
               inject: [ConfigService],
             }),
