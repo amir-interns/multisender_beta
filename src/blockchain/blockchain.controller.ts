fb991ceaccc2456edb33cb7ccf4c31a6da51da0e
desmond0002 <grazyjack83@gmail.com>
2021-11-26 14:54:17 +0300

    added enum for services

--- src/blockchain/blockchain.controller.ts
+++ src/blockchain/blockchain.controller.ts
@@ -12,6 +12,14 @@ interface IBlockchainService {
   getBalance(address: string): object;
 }
 
+enum Service {
+  Bitcoin = 'btc',
+  Ethereum = 'eth',
+  ERC20 = 'usdt',
+  TRC20 = 'trc20',
+  Tron = 'trx'
+}
+
 @Controller('blockchain')
 export class BlockchainController {
     constructor(private bitcoinService: BitcoinService,
@@ -23,13 +31,63 @@ export class BlockchainController {
     @UseGuards(JwtAuthGuard)
     @Post('balance/:type/:address')
     async getBlockchainBalance(@Param('type') type, @Param('address') address): Promise<any> {
-      const service: IBlockchainService = type === 'eth' ? this.ethereumService : (type === 'btc' ? this.bitcoinService : (type === 'usdt' ? this.usdtService : ( type === 'trx' ? this.trxService : this.trc20Service)))
+      let service: IBlockchainService 
+      switch(type) { 
+        case Service.Ethereum: { 
+          service = this.ethereumService
+          break; 
+        } 
+        case Service.Bitcoin: { 
+          service = this.bitcoinService
+          break; 
+        } 
+        case Service.ERC20: {
+          service = this.usdtService
+          break;    
+        } 
+        case Service.TRC20: { 
+          service = this.trc20Service
+          break; 
+        }
+        case Service.Tron: { 
+          service = this.trxService
+          break; 
+        }    
+        default: { 
+          throw new Error("Invalid request");              
+        } 
+      }
       return await service.getBalance(address)
     }
     @UseGuards(JwtAuthGuard)
     @Post('sendTx')
     async sendBlockchainTx(@Body() params: any):Promise<void>{
-      const serviceType = params.type === 'eth' ? this.ethereumService : (params.type === 'btc' ? this.bitcoinService : (params.type === 'usdt' ? this.usdtService : ( params.type === 'trx' ? this.trxService : this.trc20Service)))
+      let serviceType: IBlockchainService  
+      switch(params.type) { 
+        case Service.Ethereum: { 
+          serviceType = this.ethereumService
+          break; 
+        } 
+        case Service.Bitcoin: { 
+          serviceType = this.bitcoinService
+          break; 
+        } 
+        case Service.ERC20: {
+          serviceType = this.usdtService
+          break;    
+        } 
+        case Service.TRC20: { 
+          serviceType = this.trc20Service
+          break; 
+        }
+        case Service.Tron: { 
+          serviceType = this.trxService
+          break; 
+        }    
+        default: { 
+          throw new Error("Invalid request");              
+        } 
+      }
       const task = new BlockchainTask(serviceType)
       return task.sendTx(params.send)
     }
